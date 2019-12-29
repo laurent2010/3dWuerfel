ff1858bbd692a7aec23171fbd99f85cc0c65cff8
Laurent Czibula <laurent.czibula@ucloud.com>
2019-12-29 13:43:33 +0100

    started coding

--- src/main.cpp
+++ src/main.cpp
@@ -2,42 +2,59 @@
 #include "WiFiServer.h"
 #include "WiFiClientSecure.h"
 
-uint8_t idx;
-uint8_t rotate;
+int xBasis = 0;
+int yBasis = 0;
+int zBasis = 150;
+int cSize = 50;
+int viewPlane = 64;
+float angle = PI / 60; 
 
-void setup() {
+float cube3d[8][3] = {
+    {xBasis - cSize, yBasis + cSize, zBasis - cSize},
+    {xBasis + cSize, yBasis + cSize, zBasis - cSize},
+    {xBasis - cSize, yBasis - cSize, zBasis - cSize},
+    {xBasis + cSize, yBasis - cSize, zBasis - cSize},
+    {xBasis - cSize, yBasis + cSize, zBasis + cSize},
+    {xBasis + cSize, yBasis + cSize, zBasis + cSize},
+    {xBasis - cSize, yBasis - cSize, zBasis + cSize},
+    {xBasis + cSize, yBasis - cSize, zBasis + cSize},
+};
+unsigned char cube2d[8][2];
+
+void setup()
+{
     // put your setup code here, to run once:
     GO.begin();
-
-    GO.lcd.println("Hello, ODROID-GO");
-
-    delay(1000);
-}
-
-void displayGO() {
     GO.lcd.clearDisplay();
-    GO.lcd.setRotation(rotate + 4);
-    GO.lcd.setCursor(30, 40);
 
-    if (idx) {
-        GO.lcd.setTextSize(1);
-        GO.lcd.setTextFont(4);
-        GO.lcd.setTextColor(MAGENTA);
-    } else {
-        GO.lcd.setTextSize(2);
-        GO.lcd.setTextFont(1);
-        GO.lcd.setTextColor(GREEN);
-    }
-    GO.lcd.print("Hello, ODROID-GO");
-
-    idx = !idx;
-    rotate++;
-    rotate = rotate % 4;
+    Serial.begin(115200);
+    Serial.println("Cube Test!");
 
     delay(1000);
 }
 
-void loop() {
+void loop()
+{
     // put your main code here, to run repeatedly:
-    displayGO();
+    xRotate3d(angle);
+    rechne3dnach2d();
+    zeichne2d();
+    GO.update();
+}
+
+void xRotate3d(float q)
+{
+    float ty, tz, temp;
+    for (byte i = 0; i < 8; i++)
+    {
+        ty = cube3d[i][1] - yBasis;
+        tz = cube3d[i][2] - zBasis;
+        temp = ty * cos(q) - tz * sin(q);
+        tz = tv * sin(q) + tz * cos(q);
+        ty = temp;
+        ty = cube3d[i][1] + yBasis;
+        tz = cube3d[i][2] + zBasis;
+
+        /* code */
+    }
 }
